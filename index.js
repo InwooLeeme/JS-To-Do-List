2058606fd44b4bc05fdd49f781383ed1b9fcbf52
ItsmeLee <arsmagica90@gmail.com>
2021-01-09 22:17:46 +0900

    Refactoring clean to do function
    Refactoring clean to do function in index.js file.

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
-const container = document.querySelector('.container');
 const form = document.querySelector('.jsForm');
 const input = document.querySelector('input');
-const ul = document.querySelector('ul');
-const toDoNumber = document.querySelector('.displayNumber');
+const ul = document.querySelector('.displayItems');
+const itemNumber = document.querySelector('.displayNumber');
 let toDos = [];
 
 function handleSubmit(event){
     event.preventDefault();
-    let currentText = input.value
+    const currentText = input.value;
     input.value = "";
-    createText(currentText);
+    createItems(currentText);
 }
 
-function createText(text){
-    const delBtn = document.createElement('span');
+function createItems(text){
     const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.addEventListener('click', cleanToDos);
     const textSpan = document.createElement('span');
-    delBtn.innerHTML = `<i class="fas fa-check-square"></i>`;
-    delBtn.addEventListener('click', deleteToDos);
-    textSpan.innerHTML = text;
-    textSpan.classList.add(toDos.length + 1);
-    textSpan.addEventListener('click',hideText);
-    ul.appendChild(li);
-    li.appendChild(delBtn);
-    li.appendChild(textSpan);
+    textSpan.addEventListener('click', () => {
+        textSpan.classList.add('hideText');
+    });
+    span.innerHTML = `<i class="fas fa-check"></i>`;
+    textSpan.innerText = text;
     const toDoObj = {
         id : toDos.length + 1,
         text : text
     }
     li.id = toDoObj.id;
+    ul.appendChild(li);
+    li.appendChild(span);
+    li.appendChild(textSpan);
     toDos.push(toDoObj);
-    toDoNumber.innerHTML = `${toDos.length} items`;
+    itemNumber.innerHTML = `${toDos.length} items`;
 }
 
-
-function deleteToDos(event){
+function cleanToDos(event){
     const li = event.target.parentNode.parentNode;
     ul.removeChild(li);
-    toDos.splice(parseInt(li.id -1), 1);
-    toDoNumber.innerHTML = `${toDos.length} items`;
+    const clearToDos = toDos.filter((toDo) => {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = clearToDos;
+    itemNumber.innerHTML = `${toDos.length} items`;
 }
 
-function hideText(event){
-    const currentTarget = event.target;
-    currentTarget.classList.add('hideText');
-}
 function init(){
     form.addEventListener('submit',handleSubmit);
 }
